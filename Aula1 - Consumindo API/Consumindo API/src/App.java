b95bb26b71350aa5df440ce3dad9ded830f53dff
Leonardo Alves De Oliveira Romero <leonardo_romero_ti@outlook.com>
2023-03-31 13:40:08 -0300

    somente os titles

---
+++ Aula1 - Consumindo API/Consumindo API/src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.security.Key;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Fazer uma conexão HTTP e buscar os top250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // Pegar só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> ListaDeFilmes = parser.parse(body);
+        
+        // Exibir e manipular os dados
+        for (Map<String,String> filme : ListaDeFilmes) {
+            System.out.println(filme.get("title"));
+        }
+
+    }
+}
